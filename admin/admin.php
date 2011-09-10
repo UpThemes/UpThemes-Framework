b46816861fc6b1041ab3b5fe822a9b976644884d
Chip Bennett <chip@chipbennett.net>
2011-09-10 15:22:20 -0500

    Add upfw_get_template_context() to framework

--- admin/admin.php
+++ admin/admin.php
@@ -282,6 +282,69 @@ function show_gallery_images(){
 
 }
 
+/**
+ * Get current template context
+ * 
+ * Returns a string containing the context of the
+ * current page template. This string is useful for several
+ * purposes, including applying an ID to the HTML
+ * body tag, and adding a contextual $name to calls
+ * to get_header(), get_footer(), get_sidebar(), 
+ * and get_template_part_file(), in order to 
+ * facilitate Child Themes overriding default Theme
+ * template part files.
+ * 
+ * @param	none
+ * @return	string	current page template context
+ */
+function upfw_get_template_context() {
+
+	$context = 'index';
+	
+	if ( is_front_page() ) {
+		// Front Page
+		$context = 'front-page';
+	} else if ( is_date() ) {
+		// Date Archive Index
+		$context = 'date';
+	} else if ( is_author() ) {
+		// Author Archive Index
+		$context = 'author';
+	} else if ( is_category() ) {
+		// Category Archive Index
+		$context = 'category';
+	} else if ( is_tag() ) {
+		// Tag Archive Index
+		$context = 'tag';
+	} else if ( is_tax() ) {
+		// Taxonomy Archive Index
+		$context = 'taxonomy';
+	} else if ( is_archive() ) {
+		// Archive Index
+		$context = 'archive';
+	} else if ( is_search() ) {
+		// Search Results Page
+		$context = 'search';
+	} else if ( is_404() ) {
+		// Error 404 Page
+		$context = '404';
+	} else if ( is_attachment() ) {
+		// Attachment Page
+		$context = 'attachment';
+	} else if ( is_single() ) {
+		// Single Blog Post
+		$context = 'single';
+	} else if ( is_page() ) {
+		// Static Page
+		$context = 'page';
+	} else if ( is_home() ) {
+		// Blog Posts Index
+		$context = 'home';
+	}
+	
+	return $context;
+}
+
 add_action('wp_ajax_show_gallery_images','show_gallery_images');
 
 /******
