256e0b0c7e01cc02745786262287425bdc26cea2
Brian Fegter <brian@liftux.com>
2011-09-20 14:27:24 -0400

    Removed default theme options check from theme options file and inserted it into render_options function

--- admin/admin.php
+++ admin/admin.php
@@ -427,6 +427,27 @@ function find_defaults($options){
 ** Render theme options
 *************************************/
 function render_options($options){
+
+	//Check if theme options set
+	global $default_check;
+	global $default_options;
+
+	if(!$default_check):
+		foreach($options as $option):
+			if($option['type'] != 'image'):
+				$default_options[$option['id']] = $option['value'];
+			else:
+				$default_options[$option['id']] = $option['url'];
+			endif;
+		endforeach;
+		$update_option = get_option('up_themes_'.UPTHEMES_SHORT_NAME);
+		if(is_array($update_option)):
+			$update_option = array_merge($update_option, $default_options);
+			update_option('up_themes_'.UPTHEMES_SHORT_NAME, $update_option);
+		else:
+			update_option('up_themes_'.UPTHEMES_SHORT_NAME, $default_options);
+		endif;
+	endif;
     
     foreach ($options as $value) {
         //Check if there are additional attributes
--- theme-options/layout-and-display_1.php
+++ theme-options/layout-and-display_1.php
@@ -1,16 +1,5 @@
 <?php
-/*  Array Options:
-   
-   name (string)
-   desc (string)
-   id (string)
-   type (string) - text, color, image, select, multiple, textarea, page, pages, category, categories
-   value (string) - default value - replaced when custom value is entered - (text, color, select, textarea, page, category)
-   options (array)
-   attr (array) - any form field attributes
-   url (string) - for image type only - defines the default image
-    
-*/
+
 $options = array (
 
     array(  "name" => "Theme Layout",
@@ -71,29 +60,6 @@ $options = array (
         
 
 );
-
-/* ------------ Do not edit below this line ----------- */
-
-//Check if theme options set
-global $default_check;
-global $default_options;
-
-if(!$default_check):
-    foreach($options as $option):
-        if($option['type'] != 'image'):
-            $default_options[$option['id']] = $option['value'];
-        else:
-            $default_options[$option['id']] = $option['url'];
-        endif;
-    endforeach;
-    $update_option = get_option('up_themes_'.UPTHEMES_SHORT_NAME);
-    if(is_array($update_option)):
-        $update_option = array_merge($update_option, $default_options);
-        update_option('up_themes_'.UPTHEMES_SHORT_NAME, $update_option);
-    else:
-        update_option('up_themes_'.UPTHEMES_SHORT_NAME, $default_options);
-    endif;
-endif;
 
 render_options($options);
 ?>
