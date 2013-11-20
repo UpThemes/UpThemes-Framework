75037b5d07aa5e43470e07583c9001692af79cd5
Chris Wallace <chris@liftux.com>
2013-11-20 15:01:40 -0600

    Moved validation function to options-sanitize.php and fixed sanitization for radio_image type.

--- library/options-sanitize.php
+++ library/options-sanitize.php
@@ -1,5 +1,109 @@
 <?php
 
+
+/**
+ * Validate and sanitize options on save
+ *
+ * This function iterates through our options, sanitizes them, and
+ * saves them to the database.
+ *
+ * @uses has_filter()
+ * @uses apply_filters()
+ *
+ */
+function upfw_options_validate( $input ) {
+
+	global $up_tabs;
+
+	// This is the "whitelist": current settings
+	$valid_input = (array) upfw_get_options();
+	// Get the array of Theme settings, by Settings Page tab
+	$settingsbytab = upfw_get_settings_by_tab();
+	// Get the array of option parameters
+	$option_parameters = upfw_get_option_parameters();
+	// Get the array of option defaults
+	$option_defaults = upfw_get_option_defaults();
+	// Get list of tabs
+
+	// Determine what type of submit was input
+	$submittype = 'submit';
+	foreach ( $up_tabs as $tab ) {
+		$resetname = 'reset-' . $tab['name'];
+		if ( ! empty( $input[$resetname] ) ) {
+			$submittype = 'reset';
+		}
+	}
+
+	// Determine what tab was input
+	$submittab = '';
+	foreach ( $up_tabs as $tab ) {
+		$submitname = 'submit-' . $tab['name'];
+		$resetname = 'reset-' . $tab['name'];
+		if ( ! empty( $input[$submitname] ) || ! empty($input[$resetname] ) ) {
+			$submittab = $tab['name'];
+		}
+	}
+
+	global $wp_customize;
+	// Get settings by tab
+	$tabsettings = ( isset ( $wp_customize ) ? $settingsbytab['all'] : $settingsbytab[$submittab] );
+
+	// Loop through each tab setting
+	foreach ( $tabsettings as $setting ) {
+
+		// If no option is selected, set the default
+		$valid_input[$setting] = ( ! isset( $input[$setting] ) ? $option_defaults[$setting] : $input[$setting] );
+
+		// If submit, validate/sanitize $input
+		if ( 'submit' == $submittype ) {
+
+			// Get the setting details from the defaults array
+			$option = $option_parameters[$setting];
+
+			// Get the array of valid options, if applicable
+			$valid_options = ( isset( $option['valid_options'] ) ? $option['valid_options'] : false );
+
+			if ( ! isset( $option['name'] ) ) {
+				continue;
+			}
+
+			if ( ! isset( $option['type'] ) ) {
+				continue;
+			}
+
+			$setting = preg_replace( '/[^a-zA-Z0-9._\-]/', '', strtolower( $option['name'] ) );
+
+			// Set checkbox to false if it wasn't sent in the $_POST
+			if ( 'checkbox' == $option['type'] && ! isset( $input[$setting] ) ) {
+				$input[$setting] = false;
+			}
+
+			// Set each item in the multicheck to false if it wasn't sent in the $_POST
+			if ( ( 'multicheck' == $option['type'] ) && ! isset( $input[$setting] ) ) {
+				foreach ( $option['valid_options'] as $key => $value ) {
+					$input[$setting][$key] = false;
+				}
+			}
+
+			// For a value to be submitted to database it must pass through a sanitization filter
+			if ( has_filter( 'upfw_sanitize_' . $option['type'] ) ) {
+				$clean[$setting] = apply_filters( 'upfw_sanitize_' . $option['type'], $input[$setting], $option );
+			}
+
+		}
+		// If reset, reset defaults
+		elseif ( 'reset' == $submittype ) {
+			// Set $setting to the default value
+			$clean[$setting] = $option_defaults[$setting];
+		}
+	}
+
+	// Hook to run after validation
+	do_action( 'upfw_after_validate', $clean );
+
+	return $clean;
+}
+
 /**
  * Option sanitization functionality is from the Options Framework: https://github.com/devinsays/options-framework-theme
  *
@@ -26,8 +130,8 @@ add_filter( 'upfw_sanitize_select', 'upfw_sanitize_enum', 10, 2 );
 
 /* Radio */
 
-//add_filter( 'upfw_sanitize_radio', 'upfw_sanitize_enum', 10, 2 );
-//add_filter( 'upfw_sanitize_radio_image', 'upfw_sanitize_enum', 10, 2 );
+add_filter( 'upfw_sanitize_radio', 'upfw_sanitize_enum', 10, 2 );
+add_filter( 'upfw_sanitize_radio_image', 'upfw_sanitize_enum', 10, 2 );
 
 /* Images */
 
