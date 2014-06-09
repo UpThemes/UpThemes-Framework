be294c56e427648f07d0dd88cbc07fcd2d523373
Chris Wallace <chris@liftux.com>
2014-06-09 11:33:45 -0500

    Fixed error in colorpicker javascript and cleaned up code formatting a bit.

--- js/up_framework.js
+++ js/up_framework.js
@@ -1,46 +1,68 @@
 ;(function($){
 
-  // Color Picker adapted from Rachel Baker's color picker example
-  // http://rachelbaker.me/how-to-use-the-new-wordpress-color-picker-in-a-plugin/
-  function pickColor(color,$el) {
-      $el.parent(".colorPickerWrapper").find('input[type="text"]').val(color);
-  }
+	// Color Picker adapted from Rachel Baker's color picker example
+	// http://rachelbaker.me/how-to-use-the-new-wordpress-color-picker-in-a-plugin/
+	function pickColor(color,$el) {
 
-  function toggle_text() {
-      colorPicker = $(".colorPickerWrapper").find('input');
-      if ("" === colorPicker.val().replace("#", "")) {
-          colorPicker.val(default_color);
-          pickColor(default_color);
-      } else {
-        pickColor(colorPicker.val());
-      }
-  }
+			if( typeof( $el ) === "object" ){
 
-  var default_color = "fbfbfb";
+				var $colorPicker = $el.parent(".colorPickerWrapper");
+
+				if( $colorPicker ){
+					$colorPicker.find('input[type="text"]').val(color);
+				}
+			}
+	}
+
+	function toggle_text() {
+
+			var colorPicker = $(".colorPickerWrapper").find('input');
+
+			if ("" === colorPicker.val().replace("#", "")) {
+
+				colorPicker.val( default_color );
+				pickColor( default_color );
+
+			} else {
+
+				pickColor( colorPicker.val() );
+
+			}
+	}
+
+	var default_color = "fbfbfb";
 
 	$(document).ready(function($){
 
-    var colorPicker = $(".colorPickerWrapper").find('input');
-    colorPicker.wpColorPicker({
-        change: function(event, ui) {
-            pickColor(colorPicker.wpColorPicker("color"),$(this));
-        },
-        clear: function() {
-            pickColor("");
-        }
-    });
+		var colorPicker = $(".colorPickerWrapper").find('input');
 
-    colorPicker.click(toggle_text);
-    if( colorPicker.length ) {
-      toggle_text();
-    }
+		colorPicker.wpColorPicker( {
 
-    $('.imageWrapper').each(function(i){
-    	if( $(this).find('input[type="text"]').val() ){
-				$(this).find('.image_preview').html('<img src="'+$(this).find('input[type="text"]').val()+'" alt="">');
-      }
-		});
+				change: function() {
+						pickColor( colorPicker.wpColorPicker("color"), $(this) );
+				},
+				clear: function() {
+						pickColor( "" );
+				}
 
-	});
+		} );
 
-})(jQuery);
+		colorPicker.click( toggle_text );
+
+		if( colorPicker.length ) {
+
+			toggle_text();
+
+		}
+
+		$('.imageWrapper').each( function(){
+
+			if( $(this).find('input[type="text"]').val() ){
+				$(this).find('.image_preview').html( '<img src="'+$(this).find('input[type="text"]').val()+'" alt="">' );
+			}
+
+		} );
+
+	} );
+
+} )(jQuery);
